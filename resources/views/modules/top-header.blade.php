4c3fa05175bba277ac3d9d6874fddd84cdbfb3db
GEEK <crew.sento0864@gmail.com>
2025-09-04 02:14:19 +0900

    top-header.blade.php を更新

--- resources/views/modules/top-header.blade.php
+++ resources/views/modules/top-header.blade.php
@@ -6,7 +6,7 @@
         </div>
         <nav class=" flex mr-5">
             @auth
-                <a href="{{ url('/dashboard') }}"
+                <a href="{{ route('contents') }}"
                     class="flex items-center justify-center mx-2 w-28 h-10 text-sm bg-main rounded-full shadow-custom hover:shadow-none transition-shadow">
                     マイページ
                 </a>
