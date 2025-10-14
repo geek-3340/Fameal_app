b8dc004040773412d5748bf87fe465ad8f03aa58
geek-3340 <crew.sento0864@gmail.com>
2025-10-15 00:32:44 +0900

    ナビメニューアクティブリンクに離乳食・買い物リストを反映

--- resources/js/modules/activeButton.js
+++ resources/js/modules/activeButton.js
@@ -3,17 +3,21 @@ export default function activeButton() {
         const currentUrl = window.location.pathname + window.location.search;
         const menuLinks = document.querySelectorAll("#js-left-menu a");
         const menuBtn = menuLinks[0];
-        const dishBtn = menuLinks[1];
+        const dishesBtn = menuLinks[1];
+        const babyfoodsBtn = menuLinks[2];
+        const shoppingListBtn = menuLinks[3];
         if (currentUrl === "/menus/month" || currentUrl === "/menus/week") {
             menuBtn.classList.remove("text-main");
             menuBtn.classList.add("text-white", "bg-main", "rounded-lg");
-            dishBtn.classList.remove("text-white", "bg-main", "rounded-lg");
-            dishBtn.classList.add("text-main");
         } else if (currentUrl === "/dishes") {
-            dishBtn.classList.remove("text-main");
-            dishBtn.classList.add("text-white", "bg-main", "rounded-lg");
-            menuBtn.classList.remove("text-white", "bg-main", "rounded-lg");
-            menuBtn.classList.add("text-main");
+            dishesBtn.classList.remove("text-main");
+            dishesBtn.classList.add("text-white", "bg-main", "rounded-lg");
+        } else if (currentUrl === "/babyfoods") {
+            babyfoodsBtn.classList.remove("text-main");
+            babyfoodsBtn.classList.add("text-white", "bg-main", "rounded-lg");
+        } else if (currentUrl === "/shopping-list") {
+            shoppingListBtn.classList.remove("text-main");
+            shoppingListBtn.classList.add("text-white", "bg-main", "rounded-lg");
         }
     });
 }
