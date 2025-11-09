20b8a3b5c2d3b2ebad11c602e0f2eaaae91fc383
GEEK <crew.sento0864@gmail.com>
2025-11-09 07:46:11 +0000

    MenusControllerリファクタリング中

--- app/Http/Controllers/MenusController.php
+++ app/Http/Controllers/MenusController.php
@@ -29,43 +29,6 @@ class MenusController extends Controller
                 $query->where('type', $type);
             })->get();
 
-        // fullcalendarに渡すデータ整形のための、空変数
-        $menusForCalendarEvents = [];
-        $dishBgColor = "";
-        $dishDisplayOrder = null;
-
-        // fullcalendar用に献立データを整形
-        foreach ($menus as $menu) {
-            switch ($menu->dish->category) {
-                case '主食':
-                case 'エネルギー':
-                    $dishBgColor = "#ffb700";
-                    $dishDisplayOrder = 0;
-                    break;
-                case '主菜':
-                case 'タンパク質':
-                    $dishBgColor = "#ff7d55";
-                    $dishDisplayOrder = 1;
-                    break;
-                case '副菜':
-                case 'ビタミン':
-                    $dishBgColor = "#91ff00";
-                    $dishDisplayOrder = 2;
-                    break;
-                default:
-                    $dishBgColor = "#bbbbbb";
-                    $dishDisplayOrder = 3;
-                    break;
-            }
-            $menusForCalendarEvents[] = [
-                'backgroundColor' => $dishBgColor,
-                'title' => $menu->dish->name . ($menu->gram ? ' ' . $menu->gram . 'g' : ''),
-                'start' => $menu->menu->date,
-                'dishDisplayOrder' => $dishDisplayOrder,
-                'category' => $menu->category,
-            ];
-        }
-
         $method='index';
         $menusForCalendarEvents=$menus->map(function($menu){
             [$dishBgColor,$dishDisplayOrder]=$this->setDishBgColorAndDisplayOrder($method,$menu->dish->category);
@@ -86,119 +49,48 @@ class MenusController extends Controller
 
     public function edit($date)
     {
-        $dishesMenuByDate = MenusDishes::with('dish', 'menu')->whereHas('menu', function ($query) use ($date) {
+
+        $menuByDate = MenusDishes::with('dish', 'menu')->whereHas('menu', function ($query) use ($date) {
             $query->where('user_id', auth()->id());
             $query->where('date', $date);
-        })->whereHas('dish', function ($query) {
-            $query->where('type', 'dish');
         })->get();
 
-        $babyFoodsMenuByDate = MenusDishes::with('dish', 'menu')->whereHas('menu', function ($query) use ($date) {
-            $query->where('user_id', auth()->id());
-            $query->where('date', $date);
-        })->whereHas('dish', function ($query) {
-            $query->where('type', 'babyfood');
-        })->get();
+        $method='edit';
 
-        $dishesMenuData = [];
-        $babyFoodsMenuData = [];
-        $dishDisplayOrder = null;
+        $dishesMenuData=[];
+        $babyFoodsMenuData=[];
 
-        foreach ($dishesMenuByDate as $menu) {
-            switch ($menu->dish->category) {
-                case '主食':
-                case 'エネルギー':
-                    $dishDisplayOrder = 0;
-                    break;
-                case '主菜':
-                case 'タンパク質':
-                    $dishDisplayOrder = 1;
-                    break;
-                case '副菜':
-                case 'ビタミン':
-                    $dishDisplayOrder = 2;
-                    break;
-                default:
-                    $dishDisplayOrder = 3;
-                    break;
+        foreach($menuByDate as $menu){
+            $dishDisplayOrder=$this->setDishBgColorAndDisplayOrder($method,$menu->dish->category);
+            if($menu->dish->type==='dish'){
+                $dishesMenuData[] = [
+                    'id' => $menu->id,
+                    'menu_category' => $menu->category,
+                    'dish_name' => $menu->dish->name,
+                    'dish_gram' => $menu->gram,
+                    'dish_recipe_url' => $menu->dish->recipe_url,
+                    'dishDisplayOrder' => $dishDisplayOrder,
+                ];
+            }elseif($menu->dish->type==='babyfood'){
+                $babyFoodsMenuData[] = [
+                    'id' => $menu->id,
+                    'menu_category' => $menu->category,
+                    'dish_name' => $menu->dish->name,
+                    'dish_gram' => $menu->gram,
+                    'dish_recipe_url' => $menu->dish->recipe_url,
+                    'dishDisplayOrder' => $dishDisplayOrder,
+                ];
             }
-            $dishesMenuData[] = [
-                'id' => $menu->id,
-                'menu_category' => $menu->category,
-                'dish_name' => $menu->dish->name,
-                'dish_gram' => $menu->gram,
-                'dish_recipe_url' => $menu->dish->recipe_url,
-                'dishDisplayOrder' => $dishDisplayOrder,
-            ];
         }
-
-        foreach ($babyFoodsMenuByDate as $menu) {
-            switch ($menu->dish->category) {
-                case '主食':
-                case 'エネルギー':
-                    $dishDisplayOrder = 0;
-                    break;
-                case '主菜':
-                case 'タンパク質':
-                    $dishDisplayOrder = 1;
-                    break;
-                case '副菜':
-                case 'ビタミン':
-                    $dishDisplayOrder = 2;
-                    break;
-                default:
-                    $dishDisplayOrder = 3;
-                    break;
-            }
-            $babyFoodsMenuData[] = [
-                'id' => $menu->id,
-                'menu_category' => $menu->category,
-                'dish_name' => $menu->dish->name,
-                'dish_gram' => $menu->gram,
-                'dish_recipe_url' => $menu->dish->recipe_url,
-                'dishDisplayOrder' => $dishDisplayOrder,
-            ];
-        }
-
-        // $menuByDate = MenusDishes::with('dish', 'menu')->whereHas('menu', function ($query) use ($date) {
-        //     $query->where('user_id', auth()->id());
-        //     $query->where('date', $date);
-        // })->get();
-
-        // $dishesMenuData=[];
-        // $babyFoodsMenuData=[];
-        // $method='edit';
-        // foreach($menuByDate as $menu){
-        //     if($menu->dish->type==='dish')
-        //     $dishDisplayOrder=$this->setDishBgColorAndDisplayOrder($method,$menu->dish->category);
-        //     $dishesMenuData[] = [
-        //         'id' => $menu->id,
-        //         'menu_category' => $menu->category,
-        //         'dish_name' => $menu->dish->name,
-        //         'dish_gram' => $menu->gram,
-        //         'dish_recipe_url' => $menu->dish->recipe_url,
-        //         'dishDisplayOrder' => $dishDisplayOrder,
-        //     ];
-        //     $babyFoodsMenuData[] = [
-        //         'id' => $menu->id,
-        //         'menu_category' => $menu->category,
-        //         'dish_name' => $menu->dish->name,
-        //         'dish_gram' => $menu->gram,
-        //         'dish_recipe_url' => $menu->dish->recipe_url,
-        //         'dishDisplayOrder' => $dishDisplayOrder,
-        //     ];
-        // }
-
-        $dishesMenuData = collect($dishesMenuData)->sortBy('dishDisplayOrder')->values()->all();
-        $babyFoodsMenuData = collect($babyFoodsMenuData)->sortBy('dishDisplayOrder')->values()->all();
 
         return response()->json([
-            'dishesByDate' => $dishesMenuData,
-            'babyFoodsByDate' => $babyFoodsMenuData,
+            'dishesByDate' => collect($dishesMenuData)->sortBy('dishDisplayOrder')->values(),
+            'babyFoodsByDate' => collect($babyFoodsMenuData)->sortBy('dishDisplayOrder')->values(),
         ]);
+
     }
 
-    public function setDishBgColorAndDisplayOrder($method,$dishCategory){
+    private function setDishBgColorAndDisplayOrder($method,$dishCategory){
         if($method==='index'){
             return match ($dishCategory) {
                 '主食', 'エネルギー' => ['#ffb700', 0],
